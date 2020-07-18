8b9bcfe31bb89de6ac73e1c8c0f9a09e4548d4a5
Camilo <camiloa.mejia@udea.edu.co>
2020-07-18 18:13:55 -0500

    Changes to port due to 3000 already taken

--- src/config/dev.js
+++ src/config/dev.js
@@ -1,6 +1,6 @@
 const baseConfigs = {
   env : process.env.ENV ? process.env.ENV : 'dev',
-  port: process.env.PORT ? process.env.PORT : 3000
+  port: process.env.PORT ? process.env.PORT : 5000
 };
 
 
