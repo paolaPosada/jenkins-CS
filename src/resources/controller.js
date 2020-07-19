6dd25b45e72ea939b51cb7b2fed6e471acfaead2
Camilo <camiloa.mejia@udea.edu.co>
2020-07-19 15:29:21 -0500

    Tests at webhook done

--- src/resources/controller.js
+++ src/resources/controller.js
@@ -1,9 +1,9 @@
-const sayHello = () => ({testing: 'Testing webhook'});
+const sayHello = () => ({message: 'Hello World!'});
 
 const sayHelloController = (req,res) => {
     const hello = sayHello()
 
-    return res.status(200).send(hello.testing)
+    return res.status(200).send(hello.message)
 }
 
 module.exports = {sayHelloController, sayHello}
